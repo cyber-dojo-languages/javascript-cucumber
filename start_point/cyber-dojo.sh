2408f1317adf83d88b8cb43bb3e2edc69b5210cf
Jon Jagger <jon@jaggersoft.com>
2017-08-21 21:33:49 +0530

    [ci skip] more failed attempts to revive javascript-cucumber

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -11,10 +11,12 @@
 #NODE_VERSION=8.2.1
 NODE_VERSION=8.4.0
 
-#
 # Use npm package 'n' to call jasmine and jshint with selected node version:
 if [ -f .jshintrc ]
   then
-    n use $NODE_VERSION /usr/lib/node_modules/jshint/bin/jshint --config .jshintrc *.js
+    n use ${NODE_VERSION} \
+      ${NODE_PATH}/jshint/bin/jshint --config .jshintrc *.js
 fi
-n use $NODE_VERSION /usr/local/lib/node_modules/cucumber/bin/cucumber.js --format-options '{"colorsEnabled":false}' *.feature
+
+n use ${NODE_VERSION} \
+  ${NODE_PATH}/cucumber/bin/cucumber.js --format-options '{"colorsEnabled":false}' *.feature
