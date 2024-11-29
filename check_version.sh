7a2bd6e51c01e33052822d69da2a8ae46bf77036
Faye <faye@kosli.com>
2024-11-29 15:44:45 +0100

    Upgrade to javascript 23.3 and cucumber 11.1

--- check_version.sh
+++ check_version.sh
@@ -5,7 +5,7 @@ readonly JSON=`cat ${MY_DIR}/docker/image_name.json`
 [[ ${JSON} =~ ${REGEX} ]]
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
-readonly EXPECTED=11.0.1
+readonly EXPECTED=11.1.0
 readonly ACTUAL=$(docker run --rm -i ${IMAGE_NAME} sh -c 'npx @cucumber/cucumber --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
